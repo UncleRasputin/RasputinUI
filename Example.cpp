f74c7b21a9be2554b32b473f01f01237dd67896e
UncleRasputin <78610840+UncleRasputin@users.noreply.github.com>
2021-02-09 07:11:10 -0600

    Update Example.cpp
    New example, with updated UI controls

--- Example.cpp
+++ Example.cpp
@@ -1,182 +1,157 @@
 #define OLC_PGE_APPLICATION
 
 #include "RasputinUI.h"
-class Example : public olc::PixelGameEngine
+
+using namespace RasputinUI;
+
+class GridBackground : public UIBackground
+{
+public:
+	olc::Pixel BackgroundColor = olc::BLANK;
+	olc::Pixel GridColor = olc::RED;
+	olc::vi2d GridSpacing = { 5,5 };
+	GridBackground(olc::Pixel backgroundColor = olc::BLANK, olc::Pixel gridColor = olc::RED, olc::vi2d gridSpacing = { 5,5 })
+	{
+		BackgroundColor = backgroundColor;
+		GridColor = gridColor;
+		GridSpacing = gridSpacing;
+	}
+
+	void Render(olc::PixelGameEngine* pge, Rect area, float fElapsedTime) override
+	{
+		pge->FillRectDecal(area.Position, area.Size, BackgroundColor);
+
+		for (int y = area.Position.y; y <= area.Position.y + area.Size.y; y += GridSpacing.y)
+		{
+			// some moving bits on the grid, a bit brighter if possible,than the original color
+			pge->FillRectDecal({ (float)area.Position.x,(float)y }, { (float)area.Size.x,1 }, GridColor);
+			int bits = rand() % 2;
+			for (int i = 0; i < bits; i++)
+			{
+				if (rand() % 3 < 2)
+				{
+					int xspot = (rand() % area.Size.x) + area.Position.x;
+					pge->FillRectDecal({ (float)xspot,(float)y }, { (float)(1 + (rand() % 4)),1 }, olc::Pixel(GridColor.r, GridColor.g, GridColor.b, 50 + (rand() % 100)));
+				}
+			}
+		}
+		for (int x = area.Position.x; x <= area.Position.x + area.Size.x; x += GridSpacing.x)
+		{
+			pge->FillRectDecal({ (float)x,(float)area.Position.y }, { 1,(float)area.Size.y }, GridColor);
+			int bits = rand() % 2;
+			for (int i = 0; i < bits; i++)
+			{
+				if (rand() % 3 < 2)
+				{
+					int yspot = (rand() % area.Size.y) + area.Position.y;
+					pge->FillRectDecal({ (float)x,(float)yspot }, { 1,(float)(1 + (rand() % 4)) }, olc::Pixel(GridColor.r, GridColor.g, GridColor.b, 50 + (rand() % 100)));
+				}
+			}
+		}
+	}
+};
+
+class Win3Window : public ControlBase
+{
+protected:
+	ControlBase* bc2;
+	ControlBase* closeButton;
+	ResizeHandle* resize;
+
+public:
+	DragHandle* windowTitle;
+
+	Win3Window(Rect location, ControlBase* parent, olc::PixelGameEngine *pge)
+		:ControlBase(location, parent)
+	{
+		Theme.Default.Background = new SolidBackground(olc::Pixel(r,g,b));
+		Theme.Default.BorderType = Enums::BorderType::Raised;
+
+		bc2 = new ControlBase({ {3,25},{Location.Size.x-6,Location.Size.y - 28} }, this);
+		bc2->Theme.Default.BorderType = Enums::BorderType::Sunken;
+
+		windowTitle = new DragHandle({ {3,3},{Location.Size.x - 6,20} }, this, pge);
+		windowTitle->Theme.Default.Padding = { 2,2,2,2 };
+		windowTitle->Theme.Default.Background = new SolidBackground(olc::DARK_BLUE);
+		windowTitle->Theme.Default.TextAlign = { Enums::Alignment::Near, Enums::Alignment::Center };
+		windowTitle->DragControl = this;
+		windowTitle->Text = "Test Window";
+
+
+		closeButton = new ControlBase({ { Location.Size.x-23,3 }, { 14,14 } }, windowTitle);
+		closeButton->Theme.Default.Background = new SolidBackground(olc::DARK_GREY);
+		closeButton->Theme.Hover.empty = false;
+		closeButton->Theme.Hover.Background = new SolidBackground(olc::GREY);
+		closeButton->Theme.Hover.ForegroundColor = olc::BLACK;
+		closeButton->Theme.Default.BorderType = Enums::BorderType::Raised;
+		closeButton->Text = "X";
+
+		auto OnClose = [&](ControlBase* sender, int mButton)
+		{
+			this->Visible = false;
+		};
+		closeButton->OnClick.Subscribe([&](ControlBase* sender, int mButton) {Visible = false;});
+
+		resize = new ResizeHandle({ {Location.Size.x-5,location.Size.y-5},{5,5} }, this, pge);
+		resize->ResizeControl = this;
+
+		this->OnResize.Subscribe([&](ControlBase* sender) {FixSize(sender); });
+
+
+		Slider* rslider = new Slider({ {10,50}, {200,20} }, this, 0, 255, r,pge);
+		Slider* gslider = new Slider({ {10,75}, {200,20} }, this, 0, 255, g, pge);
+		Slider* bslider = new Slider({ {10,100}, {200,20} }, this, 0, 255, b, pge);
+		
+		rslider->OnValue.Subscribe([&](ControlBase * sender, int val) {
+			r = val;
+			((SolidBackground*)Theme.Default.Background)->Color = olc::Pixel(r, g, b);
+		});
+		gslider->OnValue.Subscribe([&](ControlBase* sender, int val) {
+			g = val;
+			((SolidBackground*)Theme.Default.Background)->Color = olc::Pixel(r, g, b);
+		});
+		bslider->OnValue.Subscribe([&](ControlBase* sender, int val) {
+			b = val;
+			((SolidBackground*)Theme.Default.Background)->Color = olc::Pixel(r, g, b);
+		});
+	}
+
+	int r = 192;
+	int g = 192;
+	int b = 192;
+	void FixSize(ControlBase* sender)
+	{
+		olc::vi2d size = sender->Size();
+		bc2->SetSize(size.x - 6, size.y - 28);
+		windowTitle->SetSize(size.x - 6, 20);
+		resize->SetPosition({ size.x - 5, size.y - 5 });
+		closeButton->SetPosition({size.x - 23, 3});
+	}
+};
+
+class UIDemo : public olc::PixelGameEngine
 {
 private:
-	RasputinUI::UIManager *uiManager;
-	void LoadSprites()
-	{
-		sprButton = new olc::Sprite("button.png");
-		decButton = new olc::Decal(sprButton);
-		sprButtonHover = new olc::Sprite("button_hover.png");
-		decButtonHover = new olc::Decal(sprButtonHover);
-		sprButtonDisabled = new olc::Sprite("button_disabled.png");
-		decButtonDisabled = new olc::Decal(sprButtonDisabled);
-	}
-	void CreateThemes()
-	{
-		ButtonTheme.Default.Background = new RasputinUI::DecalBackground(decButton);
-		ButtonTheme.Default.ForegroundColor = olc::GREEN;
-		ButtonTheme.Default.TextScale = { 1,1 };
-
-		ButtonTheme.Hover = ButtonTheme.Default.DeepCopy();
-		ButtonTheme.Hover.Background = new RasputinUI::DecalBackground(decButtonHover);
-		ButtonTheme.Hover.ForegroundColor = olc::BLACK;
-
-		ButtonTheme.Disabled = ButtonTheme.Default.DeepCopy();
-		ButtonTheme.Disabled.Background = new RasputinUI::DecalBackground(decButtonDisabled);
-		ButtonTheme.Disabled.ForegroundColor = olc::VERY_DARK_GREY;
-
-		
-		TextBarTheme.Default.Background = new RasputinUI::SolidBackground(olc::DARK_GREY);
-		TextBarTheme.Default.Padding = { 4,4,4,4 };
-		TextBarTheme.Default.ForegroundColor = olc::WHITE;
-
-		TextBarTheme.Hover = TextBarTheme.Default.DeepCopy();
-		TextBarTheme.Hover.Background = new RasputinUI::SolidBackground(olc::GREY);
-
-		ListItemTheme.Default.Background = new RasputinUI::SolidBackground(olc::Pixel(0, 0, 0, 20));
-		ListItemTheme.Default.Padding = { 4,4,4,4 };
-		ListItemTheme.Default.ForegroundColor = olc::WHITE;
-
-		ListItemTheme.Hover = ListItemTheme.Default.DeepCopy();
-		ListItemTheme.Hover.Background = new RasputinUI::SolidBackground(olc::Pixel(0, 0, 0, 50));
-
-		ListItemTheme.Active = ListItemTheme.Default.DeepCopy();
-		ListItemTheme.Active.Background = new RasputinUI::SolidBackground(olc::RED);
-	}
+	RasputinUI::UIManager* uiManager;
 public:
-	Example()
+	UIDemo()
 	{
-		sAppName = "Rasputin UI Test App";
+		sAppName = "RasputinUI Demo App";
 	}
-
-
-	olc::Sprite* sprButton;
-	olc::Sprite* sprButtonHover;
-	olc::Decal* decButton;
-	olc::Decal* decButtonHover;
-
-	olc::Sprite* sprButtonDisabled;
-	olc::Decal* decButtonDisabled;
-public:
-	RasputinUI::ControlTheme ListItemTheme;
-	RasputinUI::ControlTheme SelectedListItemTheme;
-	RasputinUI::ControlTheme ButtonTheme;
-	RasputinUI::ControlTheme TextBarTheme;
-	RasputinUI::BorderedControl* UIWindow;
-	RasputinUI::ControlBase* Heading;
-	RasputinUI::ListControl* ListBox1;
+	
 	bool OnUserCreate() override
 	{
-		LoadSprites();
-		CreateThemes();
 		uiManager = new RasputinUI::UIManager(this);
-		CreateUI();
+		olc::Pixel gridcolor = olc::RED;
+		gridcolor.a = 40;
+		uiManager->Theme.Default.Background = new GridBackground(olc::BLACK, gridcolor, { 20,20 });
+		
+		Win3Window* win1 = new Win3Window({ {10,10},{300,200} }, uiManager, this);
+		win1->Theme.Default.Background = new SolidBackground(olc::RED);
+		Win3Window* win2 = new Win3Window({ {320,10},{300,200} }, uiManager, this);
+
 		Clear(olc::BLACK);
 		return true;
-	}
-
-	void CreateUI()
-	{
-		UIWindow = new RasputinUI::BorderedControl({ {20,10},{600,450} }, uiManager, olc::Pixel(151, 115, 106), 3);
-		uiManager->AddControl(UIWindow);
-		//UIWindow->Theme.Default.BackgroundColor = olc::Pixel(51, 115, 106);
-		UIWindow->Theme.Default.Background = new RasputinUI::SolidBackground(olc::Pixel(51, 115, 106));
-		UIWindow->Theme.Default.ForegroundColor = olc::WHITE;
-		UIWindow->Theme.Default.Padding = { 4,4,4,4 };
-		UIWindow->Theme.Default.TextAlign = { RasputinUI::Far, RasputinUI::Far };
-
-		Heading = uiManager->CreateControl({ {10,10}, {580,80} }, UIWindow);
-		Heading->Theme.Default.Background= RasputinUI::SolidBackground::BLANK;
-		//Heading->Theme.Default.BackgroundColor = olc::BLANK;
-		Heading->Theme.Default.ForegroundColor = olc::RED;
-		Heading->Theme.Default.TextAlign = { RasputinUI::Center,RasputinUI::Center };
-		Heading->Text = "Sample Heading!";
-		Heading->Theme.Default.TextScale = { 4,4 };
-
-		ListBox1 = new RasputinUI::ListControl({ {10,250}, {250, 100} }, UIWindow, ListItemTheme);
-		ListBox1->BorderColor = olc::BLACK;
-		ListBox1->BorderWidth = 3;
-		ListBox1->Theme.Default.Padding = { 5,5,5,5 };
-		
-		//ListBox1->Theme.Default.BackgroundColor = olc::Pixel(0,0,0,20);
-		ListBox1->Theme.Default.Background = new RasputinUI::SolidBackground(olc::Pixel(0, 0, 0, 20));
-		ListBox1->ItemHeight = 20;
-		uiManager->AddControl(ListBox1);
-		std::vector<std::string> items;
-		items.push_back("item 1");
-		items.push_back("item 2");
-		items.push_back("item 3");
-		items.push_back("item 4");
-		ListBox1->SetItems(items);
-
-		ListBox1->SetSelection("item 3");
-		ListBox1->SelectionChanged = std::bind(&Example::OnSelectionChanged, this, std::placeholders::_1 );
-		
-		for (int i = 0; i < 3; i++)
-		{
-			RasputinUI::ControlBase* textbar = new RasputinUI::ControlBase({ {10,100 + (i * 30)}, {580,30} }, TextBarTheme, UIWindow);
-			uiManager->AddControl(textbar);
-			textbar->Text = "This is TextBar" + std::to_string(i + 1) + "!";
-			textbar->Theme.Default.TextAlign.Horizontal = textbar->Theme.Hover.TextAlign.Horizontal = (RasputinUI::Alignment)i;
-		}
-
-		for (int i = 0; i < 4; i++)
-		{
-			RasputinUI::ControlBase* button = uiManager->CreateControl({ { 80 + (110 * i), 400 }, {100,30} }, ButtonTheme, UIWindow);
-			uiManager->AddControl(button);
-			button->Text = std::to_string(i + 1);
-			button->OnClick = std::bind(&Example::OnClick, this, std::placeholders::_1, std::placeholders::_2);
-			if (i == 2)
-				button->Enabled = false;
-		}
-
-		/*
-		RasputinUI::Flasher *flasher = new RasputinUI::Flasher({ { 300,250 },{4,50} }, UIWindow);
-		uiManager->AddControl(flasher);
-		*/
-
-		RasputinUI::TextEdit* textedit = new RasputinUI::TextEdit({ {300,320},{200,25} }, UIWindow, olc::BLACK, 2);
-		//textedit->Theme.Default.BackgroundColor = olc::WHITE;
-		textedit->Theme.Default.Background = new RasputinUI::SolidBackground(olc::WHITE);
-		textedit->Theme.Default.ForegroundColor = olc::BLACK;
-		textedit->Theme.Default.Padding = { 4,8,4,8 };
-		textedit->Theme.Default.TextAlign = { RasputinUI::Alignment::Near, RasputinUI::Alignment::Center };
-		textedit->Text = "test";
-		textedit->OnTextChanged = std::bind(&Example::OnTextChanged, this, std::placeholders::_1, std::placeholders::_2);
-		uiManager->AddControl(textedit);
-	}
-
-	void OnSelectionChanged(RasputinUI::ListControl* list)
-	{
-		Heading->Text = list->GetSelection();
-	}
-
-	void OnTextChanged(RasputinUI::ControlBase* sender, std::string val)
-	{
-		Heading->Text = val;
-	}
-	void OnClick(RasputinUI::ControlBase* sender, int mbutton)
-	{
-		if (sender->Text.compare("1") == 0)
-		{
-			ListBox1->Scroll(1);
-		}
-		if (sender->Text.compare("2") == 0)
-		{
-			ListBox1->Scroll(-1);
-		}
-		Heading->Text = "";
-		if (mbutton == 0)
-			Heading->Text += "Left ";
-		else if (mbutton == 1)
-			Heading->Text += "Right ";
-		else if (mbutton == 2)
-			Heading->Text += "Middle ";
-		Heading->Text += "click on ";
-		Heading->Text += sender->Text;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
@@ -188,9 +163,11 @@ public:
 
 int main()
 {
-	Example demo;
-	if (demo.Construct(640,480,1,1))
+	srand((unsigned int)time(NULL));
+	
+	UIDemo demo;
+	if (demo.Construct(640, 480, 1, 1))
 		demo.Start();
+
 	return 0;
 }
-
